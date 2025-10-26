48167fd7832e031c4c4a8635b8f21ec97a731557
Alex-Ustas <alex.ustas@internet.ru>
2025-10-26 11:55:28 +0300

    init version

---
+++ test.py
@@ -0,0 +1,14 @@
+# print('Python_крутой_язык!')
+#
+# print('Python', 'крутой', 'язык', sep='_', end='')
+# print('!')
+#
+# print('Python', 'крутой', 'язык', '!', sep='_')
+# print('Python', 'крутой', 'язык!', sep='_', end='!\n')
+# print('Python_', 'крутой_', 'язык!', sep='', end='')
+
+a = 17 // (23 % 7)
+b = 34 % a * 5 - 17 // 2 ** 2
+print(a, b)
+print(a * b)
+
