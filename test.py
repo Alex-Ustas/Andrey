2ba1a5e8647c3a170ec85c2622c6de4c5d5459ea
Alex-Ustas <alex.ustas@internet.ru>
2025-10-31 11:48:04 +0300

    add lesson05

--- test.py
+++ test.py
@@ -7,8 +7,24 @@
 # print('Python', 'крутой', 'язык!', sep='_', end='!\n')
 # print('Python_', 'крутой_', 'язык!', sep='', end='')
 
-a = 17 // (23 % 7)
-b = 34 % a * 5 - 17 // 2 ** 2
-print(a, b)
-print(a * b)
+# a = 17 // (23 % 7)
+# b = 34 % a * 5 - 17 // 2 ** 2
+# print(a, b)
+# print(a * b)
 
+# word = input()
+# if word == 'Python':
+#     print('Yes')
+# print('No')
+
+num = int(input())
+last_digit = num % 10    # последняя цифра числа
+first_digit = num // 10  # первая цифра числа
+if last_digit == first_digit:
+    print('ДА')
+else:
+    print('НЕТ')
+
+age = int(input())
+if 3 <= age <= 10:
+    print('Вы ребёнок')
