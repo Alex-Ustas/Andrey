cc4239cbd8f5842e389d68f51abd26500f65f8a2
Alex-Ustas <alex.ustas@internet.ru>
2026-03-29 14:07:17 +0300

    complete lesson18, add lesson19

---
+++ lesson19/exercise19_04.py
@@ -0,0 +1,23 @@
+# На вход программе подаётся строка, содержащая английский текст. Напишите программу, которая подсчитывает общее
+# количество артиклей a, an, the и выводит текст в следующем формате:
+# Общее количество артиклей: <total>
+#
+# Формат входных данных
+# На вход программе подаётся строка, содержащая английский текст. Слова текста разделены символом пробела.
+#
+# Формат выходных данных
+# Программа должна вывести общее количество артиклей a, an, the вместе с поясняющим текстом.
+#
+# Примечание. Артикли могут начинаться с заглавной буквы A, An, The.
+#
+# Пример:
+# Входные данные:
+# William Shakespeare was born in the town of Stratford, England, in the year 1564. When he was a young man, Shakespeare moved to the city of London, where he began writing plays. His plays were soon very successful, and were enjoyed both by the common people of London and also by the rich and famous. In addition to his plays, Shakespeare wrote many short poems and a few longer poems. Like his plays, these poems are still famous today.
+# Выходные данные:
+# Общее количество артиклей: 7
+s = input().split()
+
+count = s.count('A') + s.count('a')
+count += s.count('An') + s.count('an')
+count += s.count('The') + s.count('the')
+print(f'Общее количество артиклей: {count}')
